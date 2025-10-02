6a538b6eee7bc8578ff3e2e09cc2b323a7d2b8ab
tomjid <tomjidhuda04@gmail.com>
2025-10-02 14:55:38 +0600

    my first cli step 2 complete

--- my CLI/index.js
+++ my CLI/index.js
@@ -1,3 +1,11 @@
 #!/usr/bin/env node
 
-console.log("Hello , This is my first CLI project");
+const args = process.argv.slice(2);
+// console.log("arguments: ", args);
+
+if (args.length === 0) {
+  console.log("Usage: my-CLI <name>");
+  process.exit(1);
+} else {
+  console.log(`Hello, ${args[0]}! how are you ?`);
+}
