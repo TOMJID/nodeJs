6ab7103fda381540148c1f4a8d3bb79f2acf4be2
tomjid <tomjidhuda04@gmail.com>
2025-10-02 15:07:14 +0600

    added interactive using readline module

--- my CLI/index.js
+++ my CLI/index.js
@@ -1,11 +1,24 @@
 #!/usr/bin/env node
+import readline from "readline";
 
-const args = process.argv.slice(2);
-// console.log("arguments: ", args);
+// const args = process.argv.slice(2);
+// // console.log("arguments: ", args);
 
-if (args.length === 0) {
-  console.log("Usage: my-CLI <name>");
-  process.exit(1);
-} else {
-  console.log(`Hello, ${args[0]}! how are you ?`);
-}
+// if (args.length === 0) {
+//   console.log("Usage: my-CLI <name>");
+//   process.exit(1);
+// } else {
+//   console.log(`Hello, ${args[0]}! how are you ?`);
+// }
+
+// adding interactive in my cli using readline
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+// asking question to user\
+rl.question("What's your name? ", (name) => {
+  console.log(name);
+  rl.close();
+});
